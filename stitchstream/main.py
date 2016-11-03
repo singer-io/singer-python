a38be552965fb3466a2d62b616d4a71ff510fbca
Christopher Merrick <chris@stitchdata.com>
2016-11-03 12:20:33 -0400

    start using json + schema

--- stitchstream/main.py
+++ stitchstream/main.py
@@ -1,6 +1,5 @@
 import sys
-
-from transit.writer import Writer
+import json
 from io import StringIO
 
 _headers_written = False
@@ -13,29 +12,25 @@ def _write_headers():
     global _headers_written
     if not _headers_written:
         _writeline('stitchstream/0.1')
-        _writeline('Content-Type: transit')
+        _writeline('Content-Type: jsonline')
         _writeline('--')
         _headers_written = True
 
-def write_records(stream_name, key_fields, records):
+def write_records(stream_name, schema, records):
     _write_headers()
     for record in records:
-        s = StringIO()
-        writer = Writer(s, "json")
-        writer.write({'type': 'RECORD',
-                      'stream': stream_name,
-                      'key_fields': key_fields,
-                      'record': record})
-        _writeline(s.getvalue().strip())
+        _writeline(json.dumps({'type': 'RECORD',
+                               'stream': stream_name,
+                               'schema': schema,
+                               'record': record}))
 
 def write_bookmark(value):
     _write_headers()
-    s = StringIO()
-    writer = Writer(s, "json")
-    writer.write({'type': 'BOOKMARK',
-                  'value': value})
-    _writeline(s.getvalue().strip())
+    _writeline(json.dumps({'type': 'BOOKMARK',
+                           'value': value}))
 
 
 if __name__ == "__main__":
-    write_records('test', ['a'], [{'a': 'b'}, {'c':'d'}])
+    write_records('test',
+                  {'properties':{'id': {'type': 'string', 'key': True}}},
+                  [{'id': 'b'}, {'id':'d'}])
