c2e8bb7eac7ac26611dca96e4a15f70cc8f14a1d
Christopher Merrick <chris@stitchdata.com>
2016-11-18 23:37:09 -0500

    bump version: 0.2.0

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="stitchstream-python",
-      version="0.1.0",
-      description="Write to the stitchstream format from Python",
+      version="0.2.0",
+      description="Write the stitchstream format from Python",
       author="Stitch",
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
       url="https://github.com/stitchdata/stitchstream-python",
       packages=find_packages())
