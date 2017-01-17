73e25af621b44e8761eb7ada55f75609ef1a1ba3
Mike DeLaurentis <miked@stitchdata.com>
2017-01-17 13:17:29 -0500

    Remove headers

--- stitchstream/main.py
+++ stitchstream/main.py
@@ -2,35 +2,22 @@ import sys
 import json
 from io import StringIO
 
-_headers_written = False
-
 def _writeline(s):
-    print(s)
+    sys.stdout.write(s + '\n')
     sys.stdout.flush()
 
-def _write_headers():
-    global _headers_written
-    if not _headers_written:
-        _writeline('stitchstream/0.1')
-        _writeline('Content-Type: jsonline')
-        _writeline('--')
-        _headers_written = True
-
 def write_records(stream_name, records):
-    _write_headers()
     for record in records:
         _writeline(json.dumps({'type': 'RECORD',
                                'stream': stream_name,
                                'record': record}))
 
 def write_schema(stream_name, schema):
-    _write_headers()
     _writeline(json.dumps({'type': 'SCHEMA',
                            'stream': stream_name,
                            'schema': schema}))
 
 def write_state(value):
-    _write_headers()
     _writeline(json.dumps({'type': 'STATE',
                            'value': value}))
 
