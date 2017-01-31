b7ed01cc0c8db1183d2f21cf0b0ed9f5b624bef0
Mike DeLaurentis <miked@stitchdata.com>
2017-01-31 14:01:53 -0500

    pep8

--- stitchstream/main.py
+++ stitchstream/main.py
@@ -4,23 +4,28 @@ import os
 import logging
 import logging.config
 
+
 def _writeline(s):
     sys.stdout.write(s + '\n')
     sys.stdout.flush()
 
+
 def write_record(stream_name, record):
     _writeline(json.dumps({'type': 'RECORD',
                            'stream': stream_name,
-                           'record': record}))    
-    
+                           'record': record}))
+
+
 def write_records(stream_name, records):
     for record in records:
         write_record(stream_name, record)
+
 
 def write_schema(stream_name, schema):
     _writeline(json.dumps({'type': 'SCHEMA',
                            'stream': stream_name,
                            'schema': schema}))
+
 
 def write_state(value):
     _writeline(json.dumps({'type': 'STATE',
@@ -33,9 +38,13 @@ def get_logger():
     logging.config.fileConfig(path)
     return logging.getLogger('root')
 
-    
+
 if __name__ == "__main__":
     write_schema('test',
-                 {'properties':{'id': {'type': 'string', 'key': True}}})
+                 {'properties': {
+                     'id': {
+                         'type': 'string',
+                         'key': True}}})
     write_records('test',
-                  [{'id': 'b'}, {'id':'d'}])
+                  [{'id': 'b'},
+                   {'id': 'd'}])
