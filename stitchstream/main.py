701548e40ffe7bd120f3e206423551e5f36ede07
Mike DeLaurentis <delaurentis@gmail.com>
2017-01-31 14:16:28 -0500

    Add write record (#4)
    * Add write_record function
    
    * Unused import
    
    * pep8

--- stitchstream/main.py
+++ stitchstream/main.py
@@ -1,24 +1,31 @@
 import sys
 import json
-from io import StringIO
 import os
 import logging
 import logging.config
+
 
 def _writeline(s):
     sys.stdout.write(s + '\n')
     sys.stdout.flush()
 
+
+def write_record(stream_name, record):
+    _writeline(json.dumps({'type': 'RECORD',
+                           'stream': stream_name,
+                           'record': record}))
+
+
 def write_records(stream_name, records):
     for record in records:
-        _writeline(json.dumps({'type': 'RECORD',
-                               'stream': stream_name,
-                               'record': record}))
+        write_record(stream_name, record)
+
 
 def write_schema(stream_name, schema):
     _writeline(json.dumps({'type': 'SCHEMA',
                            'stream': stream_name,
                            'schema': schema}))
+
 
 def write_state(value):
     _writeline(json.dumps({'type': 'STATE',
@@ -31,9 +38,13 @@ def get_logger():
     logging.config.fileConfig(path)
     return logging.getLogger('root')
 
-    
+
 if __name__ == "__main__":
     write_schema('test',
-                 {'properties':{'id': {'type': 'string', 'key': True}}})
+                 {'properties': {
+                     'id': {
+                         'type': 'string',
+                         'key': True}}})
     write_records('test',
-                  [{'id': 'b'}, {'id':'d'}])
+                  [{'id': 'b'},
+                   {'id': 'd'}])
