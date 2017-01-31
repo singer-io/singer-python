088f6d03d763ffae360be149d29561a83f0dce7f
Mike DeLaurentis <miked@stitchdata.com>
2017-01-31 14:00:06 -0500

    Add write_record function

--- stitchstream/main.py
+++ stitchstream/main.py
@@ -9,11 +9,14 @@ def _writeline(s):
     sys.stdout.write(s + '\n')
     sys.stdout.flush()
 
+def write_record(stream_name, record):
+    _writeline(json.dumps({'type': 'RECORD',
+                           'stream': stream_name,
+                           'record': record}))    
+    
 def write_records(stream_name, records):
     for record in records:
-        _writeline(json.dumps({'type': 'RECORD',
-                               'stream': stream_name,
-                               'record': record}))
+        write_record(stream_name, record)
 
 def write_schema(stream_name, schema):
     _writeline(json.dumps({'type': 'SCHEMA',
