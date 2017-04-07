607ccdf238912744a2db9b96d8b8100aee94dbd5
Joshua Karstendick <jkarstendick@rjmetrics.com>
2017-04-07 16:17:46 -0400

    make pylint happy

--- singer/transform.py
+++ singer/transform.py
@@ -24,13 +24,13 @@ def _transform_datetime(value, integer_datetime_fmt):
                                     UNIX_MILLISECONDS_INTEGER_DATETIME_PARSING]:
         raise Exception("Invalid integer datetime parsing option")
 
-    if integer_datetime_fmt==NO_INTEGER_DATETIME_PARSING:
+    if integer_datetime_fmt == NO_INTEGER_DATETIME_PARSING:
         return string_to_datetime(value)
     else:
         try:
-            if integer_datetime_fmt==UNIX_SECONDS_INTEGER_DATETIME_PARSING:
+            if integer_datetime_fmt == UNIX_SECONDS_INTEGER_DATETIME_PARSING:
                 return unix_seconds_to_datetime(value)
-            elif integer_datetime_fmt==UNIX_MILLISECONDS_INTEGER_DATETIME_PARSING:
+            elif integer_datetime_fmt == UNIX_MILLISECONDS_INTEGER_DATETIME_PARSING:
                 return unix_milliseconds_to_datetime(value)
         except:
             return string_to_datetime(value)
@@ -40,41 +40,42 @@ UNIX_SECONDS_INTEGER_DATETIME_PARSING = 1
 UNIX_MILLISECONDS_INTEGER_DATETIME_PARSING = 2
 
 def _transform(data, typ, schema, integer_datetime_fmt):
-    if "format" in schema and typ != "null":
-        if schema["format"] == "date-time":
-            data = _transform_datetime(data, integer_datetime_fmt)
-
-    elif typ == "object":
-        data = _transform_object(data, schema["properties"])
-
-    elif typ == "array":
-        data = _transform_array(data, schema["items"])
-
-    elif typ == "null":
+    if typ == "null":
         if data is None or data == "":
             return None
         else:
             raise ValueError("Not null")
 
+    elif schema.get("format") == "date-time":
+        return _transform_datetime(data, integer_datetime_fmt)
+
+    elif typ == "object":
+        return _transform_object(data, schema["properties"])
+
+    elif typ == "array":
+        return _transform_array(data, schema["items"])
+
     elif typ == "string":
         if data:
-            data = str(data)
+            return str(data)
+        else:
+            raise ValueError("Not string")
 
     elif typ == "integer":
         if isinstance(data, str):
             data = data.replace(',', '')
-        data = int(data)
+        return int(data)
 
     elif typ == "number":
         if isinstance(data, str):
             data = data.replace(',', '')
-        data = float(data)
+        return float(data)
 
     elif typ == "boolean":
-        data = bool(data)
+        return bool(data)
 
-    return data
-
+    else:
+        raise Exception("Invalid type: {}".format(typ))
 
 def transform(data, schema, integer_datetime_fmt=NO_INTEGER_DATETIME_PARSING):
     types = schema["type"]
@@ -88,7 +89,7 @@ def transform(data, schema, integer_datetime_fmt=NO_INTEGER_DATETIME_PARSING):
     for typ in types:
         try:
             return _transform(data, typ, schema, integer_datetime_fmt)
-        except Exception as e:
+        except:
             pass
 
     raise Exception("Invalid data: {} does not match {}".format(data, schema))
