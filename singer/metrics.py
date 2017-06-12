5df721e7c81a29dd9d1204f47e79f44071e989bf
Mike DeLaurentis <miked@stitchdata.com>
2017-06-12 16:29:24 -0400

    Pylint

--- singer/metrics.py
+++ singer/metrics.py
@@ -1,92 +1,7 @@
 '''Utilities for logging and parsing metrics.
 
-A Tap should use this library to log a structured message about each read
-operation it makes. The message is a flat map, with a small key set,
-designed to be easy to convert to a metric for some popular monitoring
-tools (AWS CloudWatch, Datadog).
-
-    record_count      Number of records fetched in this operation
-    byte_count        Number of bytes fetched in this operation
-    duration          Duration of the operation in seconds
-    source            Source of the data, as a string
-    status            Either 'running', 'succeeded', or 'failed'
-    http_status_code  The HTTP status code of the response
-
-All fields are considered optional. A Tap should only include fields that
-are relevant to the operation and that it can reliably compute.
-
-If you use the singer.metrics.Timer() context manager, you'll automatically
-get status and duration. Status is 'running' while we're in the context
-manager, 'succeeded' after we exit the context manager if no exception was
-raised, and 'failed' if an exception was raised. Duration is the time in
-seconds that the thread spent inside the context manager.
-
-    >>> with singer.metrics.Timer() as timer:
-    >>>    # Do some stuff
-
-This will log a message like:
-
-    {"duration": 1.23,
-     "status": "succeeded"}
-
-You can also include a record count and the HTTP status code (when
-applicable) simply by setting properties on the metrics object.
-
-    >>> with singer.metrics.Timer(source='orders') as timer:
-    >>>     resp = requests.get('http://myapi.com/orders')
-    >>>     timer.http_status_code = resp.status_code
-    >>>     resp.raise_for_status()
-    >>>     timer.record_count = len(resp.json()['orders'])
-
-This will log a message like:
-
-    {"duration": 1.23,
-     "record_count": 234,
-     "source": "orders",
-     "status": "succeeded",
-     "http_status_code": 200}
-
-If your Tap gets records continuously rather than in discrete batches, you
-can use a singer.metrics.Counter to periodically emit metrics messages.
-
-    >>> with singer.metrics.Counter(source='orders', interval=30) as counter:
-    >>>     for order in some_client.orders():
-    >>>         counter.add(record_count=1)
-
-This would print a message about every 30 seconds indicating the current
-status and how many records have been fetched since the last message was
-logged.
-
-    {
-        "metrics": {
-            "record_count": 1234
-        },
-        "tags": {
-            "status": "running"
-        }
-    }
-    {
-        "metrics": {
-            "record_count": 1111
-        },
-        "tags": {
-            "status": "running"
-        }
-    }
-    {
-        "metrics": {
-            "record_count": 1212
-        },
-        "tags": {
-            "status": "succeeded"
-        }
-    }
-
-It is recommended to use a short name for the source (such as "orders")
-rather than a full URL, so that an application consuming the logs can
-easily group together metrics related to requests to the same logical
-source.
-
+A Tap should use this library to log structured messages about the read
+operations it makes.
 '''
 
 import json
@@ -95,18 +10,18 @@ import re
 import time
 import logging
 
+DEFAULT_LOG_INTERVAL = 60
 
 class Status:  # pylint: disable=too-few-public-methods
     '''Constants for status codes'''
     succeeded = 'succeeded'
-    running = 'running'
     failed = 'failed'
 
 class Metric:  # pylint: disable=too-few-public-methods
     '''Constants for metric names'''
+
     record_count = 'record_count'
-    byte_count = 'byte_count'
-    duration = 'duration'
+    job_duration = 'job_duration'
     http_request_duration = 'http_request_duration'
     http_request_count = 'http_request_count'
 
@@ -118,6 +33,15 @@ class Tag:  # pylint: disable=too-few-public-methods
 
 
 def log_metric(logger, metric_type, metric, value, tags):
+    '''Log a single metric.
+
+    Args:
+      logger:      The logger to log to
+      metric_type: Type of the metric, either 'counter' or 'timer'
+      value:       The value of the metric
+      tags:        Dict of tag names / values
+
+    '''
     result = {
         'type': metric_type,
         'metric': metric,
@@ -127,10 +51,32 @@ def log_metric(logger, metric_type, metric, value, tags):
     logger.info('METRIC: %s', json.dumps(result))
 
 
-DEFAULT_LOG_INTERVAL = 60
 
+class Counter(object):  # pylint: disable=too-few-public-methods
+    '''Increments a counter metric.
 
-class ItemCounter(object):  # pylint: disable=too-few-public-methods
+    When you use Counter as a context manager, it will automatically emit
+    "counter" metrics periodically and also when the context exits. The
+    only thing you need to do is initialize the Counter and then call
+    increment().
+
+    >>> with singer.metrics.Counter(metric='record_count', endpoint='users') as counter:
+    >>>    for user in get_users(...):
+    >>>        # Print out the user
+    >>>        counter.increment()
+
+    This would print a metric like this:
+
+    {
+      "type":   "counter",
+      "metric": "record_count",
+      "value":   12345,
+      "tags": {
+        "endpoint": "users",
+      }
+    }
+
+    '''
 
     def __init__(self, metric, endpoint=None, log_interval=DEFAULT_LOG_INTERVAL):
         self.metric = metric
@@ -164,36 +110,52 @@ class ItemCounter(object):  # pylint: disable=too-few-public-methods
         return time.time() - self.last_log_time > self.log_interval
 
 
-class OperationCounter(object):  # pylint: disable=too-few-public-methods
+class Timer(object):  # pylint: disable=too-few-public-methods
+    '''Produces metrics about the duration and number of operations.
 
-    def __init__(self, metric, endpoint=None):
-        self.metric = metric
+    You use a Timer as a context manager wrapping around some operation.
+    When the context exits, the Timer emits two metrics:
+
+      1. A "timer" metric that includes the duration
+      2. A "counter" metric with value=1 (to count number of operations)
+
+    It will automatically include a "status" tag that is "failed" if the
+    context exits with an Exception or "success" if it exits cleanly. You
+    can override this by setting timer.status within the context.
+
+    >>> with singer.metrics.Timer(duration_metric="request_duration",
+    >>>                           counter_metric="request_count",
+    >>>                           endpoint="users"):
+    >>>    # Do some stuff
+
+    This produces two metrics:
+
+    {
+      "type": "timer",
+      "metric": "request_duration",
+      "value": 1.23,
+      "tags": {
+        "endpoint": "users",
+        "status": "success"
+      }
+    },
+    {
+      "type": "counter",
+      "metric": "request_count",
+      "value": 4567,
+      "tags": {
+        "endpoint": "users",
+        "status": "success"
+      }
+    }
+
+    '''
+    def __init__(self, duration_metric, counter_metric, endpoint):
+        self.duration_metric = duration_metric
+        self.counter_metric = counter_metric
         self.endpoint = endpoint
         self.http_status_code = None
-        self.logger = logging.getLogger(__name__)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        tags = {}
-        if self.endpoint:
-            tags[Tag.endpoint] = self.endpoint
-        if exc_type is None:
-            tags[Tag.status] = Status.succeeded
-        else:
-            tags[Tag.status] = Status.failed
-        if self.http_status_code:
-            tags[Tag.http_status_code] = self.http_status_code
-        log_metric(self.logger, 'counter', self.metric, 1, tags)
-
-
-class Timer(object):  # pylint: disable=too-few-public-methods
-    '''Captures timing metrics and logs them.'''
-
-    def __init__(self, metric, endpoint):
-        self.metric = metric
-        self.endpoint = endpoint
+        self.status = None
         self.logger = logging.getLogger(__name__)
         self.start_time = None
 
@@ -201,27 +163,51 @@ class Timer(object):  # pylint: disable=too-few-public-methods
         self.start_time = time.time()
         return self
 
+    def elapsed(self):
+        '''Return elapsed time'''
+        return time.time() - self.start_time
+
     def __exit__(self, exc_type, exc_value, traceback):
-        duration = time.time() - self.start_time,
         tags = {}
-        if self.endpoint:
-            tags[Tag.endpoint] = self.endpoint
-        log_metric(self.logger, 'timer', self.metric, duration, tags)
+
+        for k in [Tag.endpoint,
+                  Tag.http_status_code,
+                  Tag.status]:
+            if self.__dict__[k] is not None:
+                tags[k] = self.__dict__[k]
+
+        if Tag.status not in tags:
+            if exc_type is None:
+                tags[Tag.status] = Status.succeeded
+            else:
+                tags[Tag.status] = Status.failed
+
+        if self.duration_metric:
+            log_metric(self.logger, 'timer', self.duration_metric, self.elapsed(), tags)
+        if self.counter_metric:
+            log_metric(self.logger, 'counter', self.counter_metric, 1, tags)
 
 
 def record_counter(endpoint=None, log_interval=DEFAULT_LOG_INTERVAL):
-    '''Returns an ItemCounter for counting records retrieved from the source'''
-    return ItemCounter(Metric.record_count, endpoint, log_interval=log_interval)
+    '''Use for counting records retrieved from the source.
+
+    >>> with singer.metrics.record_counter("users") as counter:
+    >>>     for user in some_source_of_users():
+    >>>         # Do something with the user
+    >>>         counter.increment()
+    '''
+    return Counter(Metric.record_count, endpoint, log_interval=log_interval)
 
 
 def http_request_timer(endpoint):
-    '''Use for timing HTTP requests to an endpoint'''
-    return Timer(Metric.http_request_duration, endpoint)
+    '''Use for timing and counting HTTP requests to an endpoint
 
-
-def http_request_counter(endpoint):
-    '''Use for counting HTTP requests to an endpoint'''
-    return OperationCounter(Metric.http_request_count, endpoint)
+    >>> with singer.metrics.http_request_timer("users") as timer:
+    >>>     # Make a request
+    '''
+    return Timer(Metric.http_request_duration,
+                 Metric.http_request_count,
+                 endpoint)
 
 
 def parse_metrics(line):
@@ -235,4 +221,3 @@ def parse_metrics(line):
         except Exception as exc:  # pylint: disable=broad-except
             logging.getLogger(__name__).warning('Error parsing metric: %s', exc)
     return result
-
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -36,44 +36,40 @@ class TestRecordCounter(unittest.TestCase):
              ('counter', 'record_count', 5, {'endpoint': 'users'})],             
             logged_metric(log_metric))
 
-class TestHttpRequestCounter(unittest.TestCase):
+class TestHttpRequestTimer(unittest.TestCase):
 
     @patch('singer.metrics.log_metric')
     def test_success(self, log_metric):
-        with metrics.http_request_counter('users') as counter:
+        timer = metrics.http_request_timer('users')
+        timer.elapsed = lambda: 0
+        with timer:
             pass
+        got = logged_metric(log_metric)
         self.assertEqual(
-            [('counter', 'http_request_count', 1, {'endpoint': 'users', 'status': 'succeeded'})],
-            logged_metric(log_metric))
+            [('timer', 'http_request_duration', 0, {'endpoint': 'users', 'status': 'succeeded'}),
+             ('counter', 'http_request_count', 1, {'endpoint': 'users', 'status': 'succeeded'})],
+            got)
 
     @patch('singer.metrics.log_metric')
     def test_success_with_http_status_code(self, log_metric):
-        with metrics.http_request_counter('users') as counter:
-            counter.http_status_code = 200
+        with metrics.http_request_timer('users') as timer:
+            timer.elapsed = lambda: 0
+            timer.http_status_code = 200
         self.assertEqual(
-            [('counter', 'http_request_count', 1, {'endpoint': 'users', 'status': 'succeeded', 'http_status_code': 200})],
+            [('timer', 'http_request_duration', 0, {'endpoint': 'users', 'status': 'succeeded', 'http_status_code': 200}),
+             ('counter', 'http_request_count', 1, {'endpoint': 'users', 'status': 'succeeded', 'http_status_code': 200})],
             logged_metric(log_metric))
 
     @patch('singer.metrics.log_metric')
     def test_failure(self, log_metric):
         try:
-            with metrics.http_request_counter('users') as counter:
+            with metrics.http_request_timer('users') as timer:
+                timer.elapsed = lambda: 0
+                timer.http_status_code = 400
                 raise ValueError('foo is not bar')
         except ValueError:
             pass
         self.assertEqual(
-            [('counter', 'http_request_count', 1, {'endpoint': 'users', 'status': 'failed'})],
+            [('timer', 'http_request_duration', 0, {'endpoint': 'users', 'status': 'failed', 'http_status_code': 400}),
+             ('counter', 'http_request_count', 1, {'endpoint': 'users', 'status': 'failed', 'http_status_code': 400})],
             logged_metric(log_metric))        
-
-        
-class TestTimer(unittest.TestCase):
-    @patch('singer.metrics.log_metric')
-    def test_success(self, log_metric):
-        with metrics.http_request_timer('users'):
-            pass
-        got = logged_metric(log_metric)
-        (_type, metric, value, tags) = logged_metric(log_metric)[0]
-        self.assertEqual('timer', _type)
-        self.assertEqual('http_request_duration', metric)
-        self.assertEqual({'endpoint': 'users'}, tags)
-
