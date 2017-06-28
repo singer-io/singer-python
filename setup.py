2cbe7d97ccec5bf24b7f839434e4236e24c845b4
John Miller <jmiller@rjmetrics.com>
2017-06-28 15:10:30 -0400

    Pass pre_hook around

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-python",
-      version='1.9.2',
+      version='1.9.3',
       description="Singer.io utility library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
