8fd3c073d00c1b778275da76540e2aa7e0d25996
Matthew Bilyeu <matt@stitchdata.com>
2017-07-31 12:36:17 -0400

    appease pylint

--- singer/bookmarks.py
+++ singer/bookmarks.py
@@ -1,19 +1,19 @@
 def ensure_bookmark_path(state, path):
     submap = state
-    for p in path:
-        if submap.get(p) == None:
-            submap[p] = {}
+    for path_component in path:
+        if submap.get(path_component) is None:
+            submap[path_component] = {}
 
-        submap = submap[p]
+        submap = submap[path_component]
     return state
 
-def write_bookmark(state, tap_stream_id, k, v):
+def write_bookmark(state, tap_stream_id, key, val):
     state = ensure_bookmark_path(state, ['bookmarks', tap_stream_id])
-    state['bookmarks'][tap_stream_id][k] = v
+    state['bookmarks'][tap_stream_id][key] = val
     return state
 
-def get_bookmark(state, tap_stream_id, k):
-    return state.get('bookmarks', {}).get(tap_stream_id, {}).get(k)
+def get_bookmark(state, tap_stream_id, key):
+    return state.get('bookmarks', {}).get(tap_stream_id, {}).get(key)
 
 def set_offset(state, tap_stream_id, offset_key, offset_value):
     state = ensure_bookmark_path(state, ['bookmarks', tap_stream_id, "offset", offset_key])
