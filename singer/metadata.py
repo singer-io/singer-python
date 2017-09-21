6b5798b9a6cbdd24ca304246d4f6e1741afe5cb4
Paul Santa Clara <psantaclara@rjmetrics.com>
2017-09-21 16:52:46 -0400

    pylint

--- singer/metadata.py
+++ singer/metadata.py
@@ -1,10 +1,20 @@
-def compile_metadata(catalog):
-    md_dict = {}
-    for mdata in catalog.get('metadata'):
-        breadcrumb = tuple(mdata['breadcrumb'])
-        if md_dict.get(breadcrumb) is None:
-            md_dict[breadcrumb] = mdata.get('metadata')
-        else:
-            md_dict[breadcrumb].update(mdata.get('metadata'))
+def deserialize(raw_metadata):
+    return {tuple(breadcrumb): metadata for breadcrumb, metadata in raw_metadata}
 
-    return md_dict
+def serialize(compiled_metadata):
+    return list(compiled_metadata.items())
+
+def delete(compiled_metadata, breadcrumb, k):
+    del compiled_metadata[breadcrumb][k]
+
+def write(compiled_metadata, breadcrumb, k, val):
+    if val is None:
+        raise Exception()
+    if breadcrumb in compiled_metadata:
+        compiled_metadata.get(breadcrumb).update({k: val})
+    else:
+        compiled_metadata[breadcrumb] = {k: val}
+    return compiled_metadata
+
+def get(compiled_metadata, breadcrumb, k):
+    return compiled_metadata.get(breadcrumb, {}).get(k)
