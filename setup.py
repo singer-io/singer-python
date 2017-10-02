427fdc7e3f9a79f7e7af75dd9f500bee9dd93c81
John Miller <jmiller@rjmetrics.com>
2017-10-02 11:11:58 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-python",
-      version='3.5.1',
+      version='3.5.3',
       description="Singer.io utility library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
