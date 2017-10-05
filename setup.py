b003911bef793fe54063397d6f90475f263070ad
John Miller <jmiller@rjmetrics.com>
2017-10-05 10:33:45 -0400

    fix requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-python",
-      version='3.5.4',
+      version='3.5.5',
       description="Singer.io utility library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
@@ -13,7 +13,7 @@ setup(name="singer-python",
           'jsonschema==2.6.0',
           'pendulum==1.2.0',
           'simplejson==3.11.1',
-          'python-dateutil==2.6.0',
+          'python-dateutil>=2.6.0',
           'backoff==1.3.2',
       ],
       packages=find_packages(),
