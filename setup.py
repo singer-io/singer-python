a8bab0f508287a54e7b9a30548480e9a145cac2a
Matthew Bilyeu <matt@stitchdata.com>
2017-10-24 15:01:12 -0400

    bump version to 4.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-python",
-      version='3.6.3',
+      version='4.0.0',
       description="Singer.io utility library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
