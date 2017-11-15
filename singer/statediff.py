fb2f4c7a2003f00ac1d71c4fc8c4b65d958d3ba5
Mike DeLaurentis <miked@stitchdata.com>
2017-11-15 10:38:52 -0500

    Working on state diffing

---
+++ singer/statediff.py
@@ -0,0 +1,18 @@
+def paths(s, base=None):
+    if base is None:
+        base = []
+
+    result = []
+    if isinstance(s, dict):
+        for k, v in sorted(s.items()):
+            result.extend(paths(v, base + [k]))
+
+    elif isinstance(s, list):
+        for i, v in enumerate(s):
+            result.extend(paths(v, base + [i]))
+
+    else:
+        result.append([base, s])
+
+    return result
+
