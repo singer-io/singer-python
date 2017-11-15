f12e3cff354595fa06401c76f4eec4e4d59d0d4d
Mike DeLaurentis <miked@stitchdata.com>
2017-11-15 10:46:58 -0500

    Use tuples

--- singer/statediff.py
+++ singer/statediff.py
@@ -1,18 +1,18 @@
 def paths(s, base=None):
     if base is None:
-        base = []
+        base = ()
 
     result = []
     if isinstance(s, dict):
         for k, v in sorted(s.items()):
-            result.extend(paths(v, base + [k]))
+            result.extend(paths(v, base + (k,)))
 
     elif isinstance(s, list):
         for i, v in enumerate(s):
-            result.extend(paths(v, base + [i]))
+            result.extend(paths(v, base + (i,)))
 
     else:
-        result.append([base, s])
+        result.append((base, s))
 
     return result
 
--- tests/test_statediff.py
+++ tests/test_statediff.py
@@ -6,15 +6,15 @@ class TestPaths(unittest.TestCase):
     def test_simple_dict(self):
 
         self.assertEqual(
-            [[['a'], 1],
-             [['b'], 2]],
+            [(('a',), 1),
+             (('b',), 2)],
             statediff.paths({'a': 1, 'b': 2}))
 
     def test_nested_dict(self):
         self.assertEqual(
-            [[['a', 'b'], 1],
-             [['a', 'c'], 2],
-             [['d', 'e'], 3]],
+            [(('a', 'b'), 1),
+             (('a', 'c'), 2),
+             (('d', 'e'), 3)],
             statediff.paths(
                 {
                     'a': {
@@ -31,23 +31,23 @@ class TestPaths(unittest.TestCase):
 
     def test_simple_array(self):
         self.assertEqual(
-            [[[0], 'blue'],
-             [[1], 'green']],
+            [((0,), 'blue'),
+             ((1,), 'green')],
             statediff.paths(
                 ['blue', 'green']))
 
     def test_nested_array(self):
         self.assertEqual(
-            [[[0, 0], 'blue'],
-             [[0, 1], 'red'],
-             [[1, 0], 'green']],
+            [((0, 0), 'blue'),
+             ((0, 1), 'red'),
+             ((1, 0), 'green')],
             statediff.paths([['blue', 'red'], ['green']]))
 
     def test_arrays_in_dicts(self):
         self.assertEqual(
-            [[['a', 0], 'blue'],
-             [['a', 1], 'red'],
-             [['b', 0], 'green']],
+            [(('a', 0), 'blue'),
+             (('a', 1), 'red'),
+             (('b', 0), 'green')],
             statediff.paths(
                 {
                     'a': ['blue', 'red'],
