6a65ba82c26e7fabed01f86dc1981352c472b09c
Mike DeLaurentis <miked@stitchdata.com>
2017-11-15 11:57:18 -0500

    Cleanup

--- singer/statediff.py
+++ singer/statediff.py
@@ -1,5 +1,10 @@
 import collections
 
+# Named tuples for holding add, change, and remove operations
+Add    = collections.namedtuple('Add',    ['path', 'newval'])
+Change = collections.namedtuple('Change', ['path', 'oldval', 'newval'])
+Remove = collections.namedtuple('Remove', ['path', 'oldval'])
+
 def paths(s, base=None):
     '''Walk a data structure and return a list of (path, value) tuples, where
     each path is the path to a leaf node in the data structure and the
@@ -24,42 +29,43 @@ def paths(s, base=None):
 
     return result
 
-Change = collections.namedtuple('Change', ['path', 'oldval', 'newval'])
-Remove = collections.namedtuple('Remove', ['path', 'oldval'])
-Add    = collections.namedtuple('Add', ['path', 'newval'])
-
-def diff(s1, s2):
+def diff(oldstate, newstate):
     '''Compare two states, returning a list of Add, Change, and Remove
     objects.
 
-    Add(path, newval) means path exists in s2 but not s1 and its value in
-    s2 is newval.
+    Add(path, newval) means path exists in newstate but not oldstate and
+    its value in newstate is newval.
 
-    Change(path, oldval, newval) means that path exists in both s1 and s2
-    but has different values. oldval is the val in s1 and newval is the
-    value in s2.
+    Change(path, oldval, newval) means that path exists in both oldstate
+    and newstate but has different values. oldval is the val in oldstate
+    and newval is the value in newstate.
     
-    Remove(path, oldval) means the path exists in s1 but not in s2, and
-    the value in s1 is oldval.
+    Remove(path, oldval) means the path exists in oldstate but not in
+    newstate, and the value in oldstate is oldval.
 
-    '''    
-    dict1 = {k: v for (k, v) in paths(s1)}
-    dict2 = {k: v for (k, v) in paths(s2)}
+    '''
 
+    # Convert oldstate and newstate from a deeply nested dict into a
+    # single-level dict, mapping a path to a value.
+    olddict = {k: v for (k, v) in paths(oldstate)}
+    newdict = {k: v for (k, v) in paths(newstate)}
+
+    # Build the list of all paths in both oldstate and newstate to iterate
+    # over.
     all_paths = set()
-    all_paths.update(set(dict1.keys()))
-    all_paths.update(set(dict2.keys()))
+    all_paths.update(set(olddict.keys()))
+    all_paths.update(set(newdict.keys()))
 
     result = []
     for path in sorted(all_paths):
-        if path in dict1:
-            if path in dict2:
-                if dict1[path] == dict2[path]:
-                    pass
+        if path in olddict:
+            if path in newdict:
+                if olddict[path] == newdict[path]:
+                    pass # Don't emit anything if values are the same
                 else:
-                    result.append(Change(path, dict1[path], dict2[path]))
+                    result.append(Change(path, olddict[path], newdict[path]))
             else:
-                result.append(Remove(path, dict1[path]))
+                result.append(Remove(path, olddict[path]))
         else:
-            result.append(Add(path, dict2[path]))
+            result.append(Add(path, newdict[path]))
     return result
