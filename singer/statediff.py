e50666f037fcdbcd98d63a675233502e89c57660
Mike DeLaurentis <miked@stitchdata.com>
2017-11-15 11:09:34 -0500

    Working on statediff

--- singer/statediff.py
+++ singer/statediff.py
@@ -1,4 +1,12 @@
+import collections
+
 def paths(s, base=None):
+    '''Walk a data structure and return a list of (path, value) tuples, where
+    each path is the path to a leaf node in the data structure and the
+    value is the value it points to. Each path will be a tuple of strings
+    and integers.
+
+    '''
     if base is None:
         base = ()
 
@@ -16,3 +24,42 @@ def paths(s, base=None):
 
     return result
 
+Change = collections.namedtuple('Change', ['path', 'oldval', 'newval'])
+Remove = collections.namedtuple('Remove', ['path', 'oldval'])
+Add    = collections.namedtuple('Add', ['path', 'newval'])
+
+def diff(s1, s2):
+    '''Compare two states, returning a list of Add, Change, and Remove
+    objects.
+
+    Add(path, newval) means path exists in s2 but not s1 and its value in
+    s2 is newval.
+
+    Change(path, oldval, newval) means that path exists in both s1 and s2
+    but has different values. oldval is the val in s1 and newval is the
+    value in s2.
+    
+    Remove(path, oldval) means the path exists in s1 but not in s2, and
+    the value in s1 is oldval.
+
+    '''    
+    dict1 = {k: v for (k, v) in paths(s1)}
+    dict2 = {k: v for (k, v) in paths(s2)}
+
+    all_paths = set()
+    all_paths.update(set(dict1.keys()))
+    all_paths.update(set(dict2.keys()))
+
+    result = []
+    for path in sorted(all_paths):
+        if path in dict1:
+            if path in dict2:
+                if dict1[path] == dict2[path]:
+                    pass
+                else:
+                    result.append(Change(path, dict1[path], dict2[path]))
+            else:
+                result.append(Remove(path, dict1[path]))
+        else:
+            result.append(Add(path, dict2[path]))
+    return result
--- tests/test_statediff.py
+++ tests/test_statediff.py
@@ -1,5 +1,6 @@
 import unittest
 import singer.statediff as statediff
+from singer.statediff import Add, Remove, Change
 
 class TestPaths(unittest.TestCase):
 
@@ -56,3 +57,24 @@ class TestPaths(unittest.TestCase):
             )
         )
                 
+class TestDiff(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(
+            [Add(('a',), 1),
+             Add(('b',), 2)],
+            statediff.diff({}, {'a': 1, 'b': 2}))
+
+    def test_remove(self):
+        self.assertEqual(
+            [Remove(('a',), 1),
+             Remove(('b',), 2)],
+            statediff.diff({'a': 1, 'b': 2}, {}))
+
+    def test_change(self):
+        self.assertEqual(
+            [Change(('a',), 1, 100),
+             Change(('b',), 2, 200)],
+            statediff.diff({'a': 1, 'b': 2},
+                           {'a': 100, 'b': 200}))
+                           
