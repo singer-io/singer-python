215171b445771623053ea649d8192e4bf4ad4a24
Nick McCoy <nick@stitchdata.com>
2017-12-14 15:42:41 -0500

    fixing strftime format string

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,5 +6,5 @@ import singer.utils as u
 
 class TestFormat(unittest.TestCase):
     def test_small_years(self):
-        self.assertEqual(u.strftime(dt(90, 1, 1, tzinfo=tz.utc)),
+        self.assertEqual(u.strftime(dt(90, 1, 1, tzinfo=tz.utc), '%04Y-%m-%dT%H:%M:%S.%fZ'),
                          "0090-01-01T00:00:00.000000Z")
