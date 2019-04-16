fa7b2a70608b3d400f49f2238454b25f6bbcb6e0
Andy <luandy64@gmail.com>
2019-04-16 12:09:59 -0400

    Add backwards compatible dict.get() fallback value (#95)

--- singer/bookmarks.py
+++ singer/bookmarks.py
@@ -22,8 +22,8 @@ def reset_stream(state, tap_stream_id):
     state['bookmarks'][tap_stream_id] = {}
     return state
 
-def get_bookmark(state, tap_stream_id, key):
-    return state.get('bookmarks', {}).get(tap_stream_id, {}).get(key)
+def get_bookmark(state, tap_stream_id, key, default=None):
+    return state.get('bookmarks', {}).get(tap_stream_id, {}).get(key, default)
 
 def set_offset(state, tap_stream_id, offset_key, offset_value):
     state = ensure_bookmark_path(state, ['bookmarks', tap_stream_id, "offset", offset_key])
@@ -35,12 +35,12 @@ def clear_offset(state, tap_stream_id):
     state['bookmarks'][tap_stream_id]["offset"] = {}
     return state
 
-def get_offset(state, tap_stream_id):
-    return state.get('bookmarks', {}).get(tap_stream_id, {}).get("offset")
+def get_offset(state, tap_stream_id, default=None):
+    return state.get('bookmarks', {}).get(tap_stream_id, {}).get("offset", default)
 
 def set_currently_syncing(state, tap_stream_id):
     state['currently_syncing'] = tap_stream_id
     return state
 
-def get_currently_syncing(state):
-    return state.get('currently_syncing')
+def get_currently_syncing(state, default=None):
+    return state.get('currently_syncing', default)
