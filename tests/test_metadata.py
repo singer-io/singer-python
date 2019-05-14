2f460f9e3ea94fc924663c5aaf0559f3a8fd3af1
Andy Lu <andy@stitchdata.com>
2019-05-14 18:22:12 +0000

    Add tests in an iteration pattern

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -432,3 +432,143 @@ class TestStandardMetadata(unittest.TestCase):
                 valid_replication_keys=test_rk
             ),
             expected_metadata)
+
+
+    def test_standard_metadata_no_schemas(self):
+
+        # Some contants shared by a number of expected metadata objects
+        test_kp = ['id']
+        test_rm = 'INCREMENTAL'
+        test_rk = ['id', 'created']
+
+        # test_variables is a list of tuples, where the first element is a
+        # dictionary of parameters for `get_standard_metadata()` and the
+        # second element is the expected metadata
+        test_variables = [
+            (
+                {
+                    schema: None,
+                    key_properties: None,
+                    replication_method: None,
+                    valid_replication_keys: None
+                },
+                [
+                    {
+                        'metadata': {},
+                        'breadcrumb': []
+                    }
+                ]
+            ),
+            (
+                {
+                    schema: None,
+                    key_properties: None,
+                    replication_method: None,
+                    valid_replication_keys: test_rk
+                },
+                [
+                    {
+                        'metadata': {
+                            'inclusion': 'available',
+                            'valid-replication-keys': ['id','created']
+                        },
+                        'breadcrumb': []
+                    }
+                ]
+            ),
+            (
+                {
+                    schema: None,
+                    key_properties: None,
+                    replication_method: test_rm,
+                    valid_replication_keys: None
+                },
+                [
+                    {
+                        'metadata': {
+                            'inclusion': 'available',
+                            'forced-replication-method': 'INCREMENTAL'
+                        },
+                        'breadcrumb': []
+                    }
+                ]
+            ),
+            (
+                {
+                    schema: None,
+                    key_properties: None,
+                    replication_method: test_rm,
+                    valid_replication_keys: test_rk
+                },
+                [
+                    {
+                        'metadata': {
+                            'inclusion': 'available',
+                            'forced-replication-method': 'INCREMENTAL',
+                            'valid-replication-keys': ['id','created']
+                        },
+                        'breadcrumb': []
+                    }
+                ]
+            ),
+            (
+                {
+                    schema: None,
+                    key_properties: test_kp,
+                    replication_method: None,
+                    valid_replication_keys: None
+                },
+                [
+                    {
+                        'metadata': {
+                            'inclusion': 'available',
+                            'table-key-properties': ['id'],
+                        },
+                        'breadcrumb': []
+                    }
+                ]
+            ),
+            (
+                {
+                    schema: None,
+                    key_properties: test_kp,
+                    replication_method: None,
+                    valid_replication_keys: test_rk
+                },
+                [
+                    {
+                        'metadata': {
+                            'inclusion': 'available',
+                            'table-key-properties': ['id'],
+                            'valid-replication-keys': ['id','created']
+                        },
+                        'breadcrumb': []
+                    }
+                ]
+            ),
+            (
+                {
+                    metadata: None,
+                    key_properties: test_kp,
+                    replication_method: test_rm,
+                    valid_replication_keys: test_rk
+                },
+                [
+                    {
+                        'metadata': {
+                            'inclusion': 'available',
+                            'table-key-properties': ['id'],
+                            'forced-replication-method': 'INCREMENTAL'
+                            'valid-replication-keys': ['id','created']
+                        },
+                        'breadcrumb': []
+                    }
+                ]
+            )
+        ]
+
+        for var in test_variables:
+            function_params = var[0]
+            expected_metadata = var[1]
+            self.assertEqual(get_standard_metadata(**function_params),
+                             expected_metadata)
