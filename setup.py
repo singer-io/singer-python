f2b383324a3a97b98d6e29f69dbadae2632dbd2f
Andy Lu <andy@stitchdata.com>
2021-02-03 09:08:43 -0600

    Bump to v5.10.0, update changelog (#139)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-python",
-      version='5.9.1',
+      version='5.10.0',
       description="Singer.io utility library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
