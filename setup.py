142049537557bea3a0e1d0f87bef1e345ef560c6
eric_lebail <eric_lebail@hotmail.com>
2021-04-02 15:17:25 +0200

    Update jsonschema version and other dependencies

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-python",
-      version='5.12.1',
+      version='5.12.2',
       description="Singer.io utility library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       url="http://singer.io",
       install_requires=[
-          'pytz==2018.4',
-          'jsonschema==2.6.0',
-          'simplejson==3.11.1',
+          'pytz>=2018.4',
+          'jsonschema>=3.0,<4.0',
+          'simplejson>=3.11.1',
           'python-dateutil>=2.6.0',
-          'backoff==1.8.0',
+          'backoff>=1.8.0',
 	  'ciso8601',
       ],
       extras_require={
