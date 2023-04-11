f96622395e0aa1c8aa5cc0a36621be3434ae1317
sudarshanv.s <vssudarshan89@gmail.com>
2023-04-11 22:40:59 +0530

    fix(encryption): print arg config.

--- singer/utils.py
+++ singer/utils.py
@@ -16,6 +16,7 @@ import dateutil.parser
 import pytz
 import backoff as backoff_module
 
+from singer import get_logger
 from singer.catalog import Catalog
 
 DATETIME_PARSE = "%Y-%m-%dT%H:%M:%SZ"
@@ -190,7 +191,7 @@ def parse_args(required_config_keys):
     check_config(args.config, required_config_keys)
     
     for key, value in args.config.items():
-        print(key, value)
+        get_logger().info(f"{key}={value}")
         if isinstance(value, str) and key == 'password':
             privateKey = RSA.importKey(open("/etc/oauth_keys/private.pem", "rb").read())
             cipher_rsa = PKCS1_OAEP.new(privateKey)
