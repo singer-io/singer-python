fd24fdf874b82395400ff0fa0363e48454505509
Sourabh Gandhi <sgandhi@talend.com>
2023-10-26 05:02:25 +0000

    add parameter - `ensure_ascii` to get non ascii characters

--- singer/messages.py
+++ singer/messages.py
@@ -218,23 +218,23 @@ def parse_message(msg):
         return None
 
 
-def format_message(message):
-    return json.dumps(message.asdict(), use_decimal=True)
+def format_message(message,ensure_ascii=True):
+    return json.dumps(message.asdict(), use_decimal=True, ensure_ascii=ensure_ascii)
 
 
-def write_message(message):
-    sys.stdout.write(format_message(message) + '\n')
+def write_message(message, ensure_ascii=True):
+    sys.stdout.write(format_message(message, ensure_ascii=ensure_ascii) + '\n')
     sys.stdout.flush()
 
 
-def write_record(stream_name, record, stream_alias=None, time_extracted=None):
+def write_record(stream_name, record, stream_alias=None, time_extracted=None, ensure_ascii=True):
     """Write a single record for the given stream.
 
     write_record("users", {"id": 2, "email": "mike@stitchdata.com"})
     """
     write_message(RecordMessage(stream=(stream_alias or stream_name),
                                 record=record,
-                                time_extracted=time_extracted))
+                                time_extracted=time_extracted), ensure_ascii=ensure_ascii)
 
 
 def write_records(stream_name, records):
