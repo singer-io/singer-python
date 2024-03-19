4d899f6f55e499d08f2242aeff58a4e5c3eff0d2
Bryant Gray <bgray@talend.com>
2024-03-19 15:56:44 +0000

    Pin minumum and major versions

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(name="singer-python",
       url="http://singer.io",
       install_requires=[
           'pytz>=2018.4',
-          'jsonschema==2.6.0',
-          'simplejson>=3.11.1,==3.*',
-          'python-dateutil>=2.6.0',
+          'jsonschema>=2.6.0,==2.*',
+          'simplejson>=3.13.2,==3.*',
+          'python-dateutil>=2.7.3,==2.*',
           'backoff>=2.2.1,==2.*',
-          'ciso8601',
+          'ciso8601>=2.3.1,==2.*',
       ],
       extras_require={
           'dev': [
