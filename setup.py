5067115a868c2c7797ffa6c2f740c170a9aa83a2
sudarshanv.s <vssudarshan89@gmail.com>
2024-06-27 17:46:28 +0530

    chore(singer-python): bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-python",
-      version='5.12.4',
+      version='5.12.5',
       description="Singer.io utility library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
