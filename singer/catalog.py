f0e1e6ec69b5c57116c3aa29a66ff7485f162fc6
Bryant Gray <bryant.gray@qlik.com>
2025-03-24 16:59:41 -0400

    Use underscore instead of dash in setup.cfg (#171)
    * Use underscore instead of dash in setup.cfg
    
    https://github.com/pypa/setuptools/issues/4910
    
    * disable some pylint warnings

--- singer/catalog.py
+++ singer/catalog.py
@@ -20,6 +20,7 @@ def write_catalog(catalog):
 # pylint: disable=too-many-instance-attributes
 class CatalogEntry():
 
+    # pylint: disable=too-many-positional-arguments
     def __init__(self, tap_stream_id=None, stream=None,
                  key_properties=None, schema=None, replication_key=None,
                  is_view=None, database=None, table=None, row_count=None,
