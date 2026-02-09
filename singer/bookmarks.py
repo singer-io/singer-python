70c53d99d516333ea5b8799fb47dda3ea7887bca
Leslie VanDeMark <lvandemark@talend.com>
2026-02-09 19:42:35 +0000

    Add activate_versions state modification fns, new state.py file -----------------------------
    Co-authored-by: Ben Allred <ben.allred@qlik.com>

--- singer/bookmarks.py
+++ singer/bookmarks.py
@@ -1,44 +1,43 @@
+from singer import state as st
+from typing_extensions import deprecated
+
+@deprecated("Use singer.state.ensure_state_path")
 def ensure_bookmark_path(state, path):
-    submap = state
-    for path_component in path:
-        if submap.get(path_component) is None:
-            submap[path_component] = {}
+    return st.ensure_state_path(state, path)
 
-        submap = submap[path_component]
-    return state
-
+@deprecated("Use singer.state.write_bookmark")
 def write_bookmark(state, tap_stream_id, key, val):
-    state = ensure_bookmark_path(state, ['bookmarks', tap_stream_id])
-    state['bookmarks'][tap_stream_id][key] = val
-    return state
+    return st.write_bookmark(state, tap_stream_id, key, val)
 
+@deprecated("Use singer.state.clear_bookmark")
 def clear_bookmark(state, tap_stream_id, key):
-    state = ensure_bookmark_path(state, ['bookmarks', tap_stream_id])
-    state['bookmarks'][tap_stream_id].pop(key, None)
-    return state
+    return st.clear_bookmark(state, tap_stream_id, key)
 
+@deprecated("Use singer.state.reset_stream")
 def reset_stream(state, tap_stream_id):
-    state = ensure_bookmark_path(state, ['bookmarks', tap_stream_id])
-    state['bookmarks'][tap_stream_id] = {}
-    return state
+    return st.reset_stream(state, tap_stream_id)
 
+@deprecated("Use singer.state.get_bookmark")
 def get_bookmark(state, tap_stream_id, key, default=None):
-    return state.get('bookmarks', {}).get(tap_stream_id, {}).get(key, default)
+    return st.get_bookmark(state, tap_stream_id, key, default)
 
+@deprecated("Use singer.state.set_offset")
 def set_offset(state, tap_stream_id, offset_key, offset_value):
-    state = ensure_bookmark_path(state, ['bookmarks', tap_stream_id, "offset", offset_key])
-    state['bookmarks'][tap_stream_id]["offset"][offset_key] = offset_value
-    return state
+    return st.set_offset(state, tap_stream_id, offset_key, offset_value)
 
+@deprecated("Use singer.state.clear_offset")
 def clear_offset(state, tap_stream_id):
-    return clear_bookmark(state, tap_stream_id, "offset")
+    return st.clear_offset(state, tap_stream_id)
 
+@deprecated("Use singer.state.get_offset")
 def get_offset(state, tap_stream_id, default=None):
-    return state.get('bookmarks', {}).get(tap_stream_id, {}).get("offset", default)
+    return st.get_offset(state, tap_stream_id, default)
 
+@deprecated("Use singer.state.set_currently_syncing")
 def set_currently_syncing(state, tap_stream_id):
-    state['currently_syncing'] = tap_stream_id
-    return state
+    return st.set_currently_syncing(state, tap_stream_id)
 
+@deprecated("Use singer.state.get_currently_syncing")
 def get_currently_syncing(state, default=None):
-    return state.get('currently_syncing', default)
+    return st.get_currently_syncing(state, default)
+
